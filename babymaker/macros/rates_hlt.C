80699c3ff30fac9a00cb4d4876e17acf6132c6fa
Manuel Franco Sevilla <manuelf@physics.ucsb.edu>
2015-03-13 13:33:37 -0700

    Improved rates_hlt.C now printing all the relevant HLT rates

--- babymaker/macros/rates_hlt.C
+++ babymaker/macros/rates_hlt.C
@@ -25,6 +25,7 @@
 #include "TH1D.h"
 #include "TH2D.h"
 #include "TMath.h"
+#include "TBox.h"
 
 #define NCuts 1
 
@@ -34,7 +35,8 @@ using std::endl;
 
 void ReadChains(vector<TChain*> &chain, vector<int> &entries, TString folder, vector<TString> filenames);
 
-void rates_hlt(TString isocut = "0.4", TString folder="root/15-03-11/el15/", bool applyCSV=false){
+void rates_hlt(TString isocut = "0.4", TString folder="root/15-03-12/el15/", 
+	       bool applyCSV=false, bool onlyRates=false){
   styles style("2Dtitle"); style.setDefaultStyle(); gStyle->SetPaintTextFormat("4.1f");
   gStyle->SetHatchesLineWidth(2);
   TCanvas can;
@@ -47,17 +49,19 @@ void rates_hlt(TString isocut = "0.4", TString folder="root/15-03-11/el15/", boo
   filenames.push_back("/W*");                   Htag.push_back("Wjets");
   filenames.push_back("/*T1tttt*1500_*PU20*");  Htag.push_back("sig1500");
   filenames.push_back("/*T1tttt*1025_*");       Htag.push_back("sig1025");
-  filenames.push_back("/*T1tttt*1200_*PU20*");  Htag.push_back("sig1200");
+  //filenames.push_back("/*T1tttt*1200_*PU20*");  Htag.push_back("sig1200");
   Htag.push_back("Total");
   vector<TChain*> chain;
   vector<int> noriginal;
   ReadChains(chain, noriginal, folder, filenames);
+  if(chain.size()==0) return;
   vector<int> indchain;
   indchain.push_back(0);
   indchain.push_back(1);
   indchain.push_back(2);
   indchain.push_back(3);
-  indchain.push_back(6);
+  indchain.push_back(4);
+  indchain.push_back(Htag.size()-1);
   const int NSam = indchain.size();
 
   // Histograms
@@ -66,19 +70,33 @@ void rates_hlt(TString isocut = "0.4", TString folder="root/15-03-11/el15/", boo
   TString yTitle = "Minimum HLT PF E_{T,miss} (GeV)";
   TString zTitle = "HLT rate (Hz)";
   TH2D *hRate[2][NCuts][20];
+  TBox box; box.SetLineColor(9); box.SetLineWidth(4); box.SetFillStyle(0); 
 
   // Cuts
-  TString Cuts[] = {"Max$(mus_pt*((mus_reliso)<isocut))>15&&not_pu"};
+  TString Cuts[] = {"Max$(mus_pt*((mus_reliso)<isocut))>50&&not_pu"};
   TString TitleCuts[] = {", RelIso_{R=0.2} < isocut"};
   if(isEl)
-    Cuts[0] = "Max$(els_pt*((els_trackiso+els_hcaliso+els_ecaliso)<isocut))>15&&not_pu";
+    Cuts[0] = "Max$(els_pt*((els_trackiso+els_hcaliso+els_ecaliso)<isocut))>50&&not_pu";
+  Cuts[0].ReplaceAll("isocut",isocut);
+  // Cuts for precise rates
+  vector<TString> cutTrigger, nameTrigger;
+  cutTrigger.push_back("onht>=600"); nameTrigger.push_back("HT600");
+  cutTrigger.push_back("onht>=400&&abs(onmet)>=70"); nameTrigger.push_back("HT400_MET70");
+  cutTrigger.push_back("onht>=400&&Max$(bjets_csv*(bjets_pt>40))>0.7"); nameTrigger.push_back("HT400_CSV");
+  cutTrigger.push_back("(0"); nameTrigger.push_back("\tAll");
+  for(unsigned icut(0); icut<cutTrigger.size()-1; icut++)
+    cutTrigger[cutTrigger.size()-1] += "||"+cutTrigger[icut];
+  cutTrigger[cutTrigger.size()-1] += ")";
+  for(unsigned icut(0); icut<cutTrigger.size(); icut++)
+    cutTrigger[icut] = "1.4e-2/19600*weight*(" + Cuts[0] + "&&" + cutTrigger[icut] + ")";
+
   if(applyCSV){
     Cuts[0] += "&&Max$(bjets_csv*(bjets_pt>40))>0.7";
     TitleCuts[0] += ", 1 b-tag";
   }
-  Cuts[0].ReplaceAll("isocut",isocut);
   TitleCuts[0].ReplaceAll("isocut",isocut);
   isocut.ReplaceAll(".","p");
+
 
   TString Tags[] = {(isEl?"el_":"mu_")+isocut+(applyCSV?"_btag":"")};
 
@@ -88,6 +106,22 @@ void rates_hlt(TString isocut = "0.4", TString folder="root/15-03-11/el15/", boo
 
   int nBinsHt = 14, nBinsMet = 14;
   float minHt=200, maxHt=900, minMet=0, maxMet=140;
+
+  /////////////////// Calculating specific trigger rates  ////////////////
+  TH1D histo1d("histo1d","",1,0,1);
+  vector<double> trigger_rate(cutTrigger.size(),0.);
+  cout<<endl;
+  for(unsigned icut(0); icut<cutTrigger.size(); icut++){
+    for(int sam(0); sam < NSam-1; sam++){
+      if(Htag[indchain[sam]].Contains("sig")) continue;
+      chain[indchain[sam]]->Project("histo1d", "onht", cutTrigger[icut]);
+      trigger_rate[icut] += histo1d.Integral(0,2);
+    } // Loop over samples
+    cout<<nameTrigger[icut]<<": "<<RoundNumber(trigger_rate[icut],1)<<"\t";
+  }
+  cout<<endl;
+  if(onlyRates) {cout<<endl; return;}
+  //////////////////////////////////////////////////////////////////////
 
   for(int icut(0); icut < NCuts; icut++){
     totCut = "1.4e-2/19600*weight*(" + Cuts[icut] + ")";
@@ -158,6 +192,10 @@ void rates_hlt(TString isocut = "0.4", TString folder="root/15-03-11/el15/", boo
       }
       hRate[0][icut][sam]->Draw("colz");
       hRate[1][icut][sam]->Draw("text same");
+      if(!applyCSV){
+	box.DrawBox(600, 0, 652, 10);
+	box.DrawBox(400, 70, 452, 80);
+      } else box.DrawBox(400, 0, 452, 10);
       can.SaveAs(Pname);
     }
   } // Loop over cuts
@@ -176,7 +214,11 @@ void ReadChains(vector<TChain*> &chain, vector<int> &entries, TString folder, ve
   bool isEl = folder.Contains("el15")?true:false;
   for(unsigned sam(0); sam < FileNames.size(); sam++){
     chain.push_back(new TChain("tree"));
-    chain[sam]->Add(folder+FileNames[sam]);
+    if(chain[sam]->Add(folder+FileNames[sam])==0){
+      cout<<folder+FileNames[sam]<<" does not exist"<<endl;
+      chain.clear();
+      return;
+    }
     if(FileNames[sam].Contains("T1tttt")) {
       int noriginal, nori_genmu0, nori_genel0;
       TChain tglobal("treeglobal");
